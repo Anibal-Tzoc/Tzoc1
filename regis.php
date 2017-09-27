55ff234392efdf0772d54b5983f4f642d8cbd656
Anibal-Tzoc <anitzoc10@gmail.com>
2017-09-27 17:06:35 -0600

    Add files via upload

---
+++ regis.php
@@ -0,0 +1,246 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Proyecto | Sistemas educacional</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="sda" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Simple Line Icons -->
+	<link rel="stylesheet" href="css/simple-line-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+<!--<link rel="stylesheet" href="css/style1.css">-->
+	</head>
+	<body>
+	<header role="banner" id="fh5co-header">
+		<div class="fluid-container">
+			<nav class="navbar navbar-default navbar-fixed-top js-fullheight">
+				<div id="navbar" class="navbar-collapse js-fullheight">
+					<ul class="nav navbar-nav navbar-left">
+						<li class="active"><a href="#" data-nav-section="home"><span>Home</span></a></li>
+												
+						<li><a href="#" data-nav-section="pricing"><span>Pricing</span></a></li>
+						<li><a href="#" data-nav-section="footer"><span>contactos</span></a></li>
+
+					</ul>
+				</div>
+			</nav>
+	  </div>
+	</header>
+
+	<section id="fh5co-home" data-section="home" style="background-image: url(images/40.jpg)"; data-stellar-background-ratio="0.5">
+		<div class="gradient"><div id="particles-js"></div></div><!--/*images/full_image_1.jpg);-->
+		<div class="container">
+			<div class="text-wrap">
+				<div class="text-inner">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<h1 class="to-animate">|Proyecto - Educacional|</h1>
+							<h2 class="to-animate">Sistema De Educacion <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">y Sitio Web</a> <br> HTML5,SQL Y CSS3 <a href="http://freehtml5.co/" target="_blank" title="Free HTML5 Bootstrap Templates" class="fh5co-link">FREEHTML5.co</a></h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<script src='particles2.min.js'></script>
+
+    <script src="js/index2.js"></script>
+
+	
+	<section id="fh5co-pricing" data-section="pricing">
+		<div class="fh5co-pricing">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 section-heading text-center">
+						<h2 class="to-animate">Plan de creacion unico</h2>
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2 subtext">
+								<h3 class="to-animate">Para alojar o crear tu proyecto educacional tenemos los siguientes planes </h3>
+								<p><a href="index.html" class="btn btn-primary">Regresar</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+	
+
+  
+
+
+
+
+
+
+				<div class="row">
+					<form  action="proc.php" method="post">
+					<br><label>Codigo:&nbsp;</label><?php $d="ela".mt_rand(1,999999);?>
+					<input type="text" name="cod" value="<?php echo "$d";?>">
+					<br><label>Usuario:&nbsp;</label><input type="text" name="pa" placeholder="Nombre..." >
+					<br><label>Nombre alumno:&nbsp;</label><input type="text" name="nom" placeholder="Nombre...">
+					<br><label>  apellido:&nbsp;&nbsp;</label><input type="text" name="apell" placeholder="Nombre...">
+					<br><label>  grado:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+					<select name="grad">
+  <option value="primero">primero</option>
+  <option value="segundo">Segundo</option>
+  <option value="tercero">tercero</option>
+  <option value="cuarto">cuarto</option>
+  <option value="quinto">quinto</option>
+  <option value="Sexto">Sexto</option>
+  <option value="septimo">septimo</option>
+  <option value="octavo">octavo</option>
+  <option value="noveno">noveno</option>
+  <option value="primer año">primer año</option>
+  <option value="segundo año">segundo año</option>
+</select>
+<br><input type="submit" name="enviar" value="Crear Cupo">
+
+
+
+						
+					</form>
+					
+
+<br>
+
+				</div>
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 col-md-push-3 to-animate">
+						<p>Las Formas de pago se establecera atravez del usuario luego de la seleccion del plan del cliente dando la participacion del usuario con objetivo de gestionamiento del gestionamiento del proyecto</p>
+						
+					
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>	
+<!---->
+	
+<section id="fh5co-footer" data-section="footer">
+	<div id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 to-animate">
+					<h3 class="section-title">Nosotros</h3>
+					<p>Cada proyecto se basa en el aumento de experiencia atravez de los usuarios al cliente dando asi una referencia a cada solicitante</p>
+					<p class="copy-right">&copy; 2015 Twist Free Template. <br>All Rights Reserved. <br>
+						Designed by <a  href="#" target="_blank" style="text-decoration: none;">FREEHTML5</a>
+						Demo Images: <a  href="http://unsplash.com/" target="_blank" style="z-index: 4;">Unsplash</a>
+					</p>
+				</div>
+
+				<div class="col-md-4 to-animate">
+					<h3 class="section-title">Nuestra direccion</h3>
+					<ul class="contact-info">
+						<li><i class="icon-map-marker"></i>198 West 21th Street, Suite 721 New York NY 10016</li>
+						<li><i class="icon-phone"></i>+ 1235 2355 98</li>
+						<li><i class="icon-envelope"></i><a href="#">info@tusitio.com</a></li>
+						<li><i class="icon-globe2"></i><a href="#">www.tusitio.com</a></li>
+					</ul>
+					<h3 class="section-title">contactanos atravez de</h3>
+					<ul class="social-media">
+						<li><a href="#" class="facebook"><i class="icon-facebook"></i></a></li>
+						<li><a href="#" class="twitter"><i class="icon-twitter"></i></a></li>
+						<li><a href="#" class="dribbble"><i class="icon-dribbble"></i></a></li>
+						<li><a href="#" class="github"><i class="icon-github-alt"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-md-4 to-animate">
+					<h3 class="section-title">comunicate</h3>
+					<form class="contact-form">
+						<div class="form-group">
+							<label for="name" class="sr-only">Name</label>
+							<input type="name" class="form-control" id="name" placeholder="Name">
+						</div>
+						<div class="form-group">
+							<label for="email" class="sr-only">Email</label>
+							<input type="email" class="form-control" id="email" placeholder="Email">
+						</div>
+						<div class="form-group">
+							<label for="message" class="sr-only">Message</label>
+							<textarea class="form-control" id="message" rows="7" placeholder="Message"></textarea>
+						</div>
+						<div class="form-group">
+							<input type="submit" id="btn-submit" class="btn btn-send-message btn-md" value="Send Message">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div></section>
+	<!-- <div id="map" class="fh5co-map"></div> -->
+
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
